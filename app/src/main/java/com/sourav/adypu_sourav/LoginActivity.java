96d908fcf8c9d8acef8bf7c10c0cce5579074588
adypu <sourav.nayak@adypu.edu.in>
2020-02-25 15:12:14 +0530

    Recycler 25-2-20

--- app/src/main/java/com/sourav/adypu_sourav/LoginActivity.java
+++ app/src/main/java/com/sourav/adypu_sourav/LoginActivity.java
@@ -11,35 +11,3 @@ package com.sourav.adypu_sourav;
         import android.widget.Button;
         import android.widget.EditText;
         import android.widget.TextView;
-
-public class Login extends AppCompatActivity {
-
-    EditText edtUsername,edtPassword;
-    TextView signUp;
-    Button btnLogin;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        edtUsername=findViewById(R.id.edtUsername);
-        edtPassword=findViewById(R.id.edtPassword);
-        signUp=findViewById(R.id.txtSignUp);
-        btnLogin=findViewById(R.id.btnLogIn);
-
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Bundle bundle=new Bundle();
-                bundle.putString("callFrom","login");
-
-                Intent intent=new Intent(Login.this,MainActivity.class);
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-        });
-
-    }
-}
---
+++ recycleractivity/src/main/java/com/sourav/recycleractivity/AndroidVersion.java
@@ -0,0 +1,22 @@
+package com.sourav.recycleractivity;
+
+public class AndroidVersion {
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    private int image;
+    private String names;
+}
